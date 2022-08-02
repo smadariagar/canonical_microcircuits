9b0186f0586934aa6e6fa055d894ce86b7556855
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2022-08-02 00:20:18 -0400

    adding params

--- assets/douglas/network_params.py
+++ assets/douglas/network_params.py
@@ -9,11 +9,59 @@ net_dict = {
         np.array([20683, 5834, 21915]),
     'neuron_params': {
         'smooth_cells': {
+            # reset membrane potential of the neurons (in mV)
+            'E_L': -65.0,
+            # threshold potential of the neurons (in mV)
+            'V_th': -50.0,
+            # membrane potential after a spike (in mV)
+            'V_reset': -65.0,
         },
         'P23': {
+            # reset membrane potential of the neurons (in mV)
+            'E_L': -65.0,
+            # threshold potential of the neurons (in mV)
+            'V_th': -50.0,
+            # membrane potential after a spike (in mV)
+            'V_reset': -65.0,
         },
         'P56': {
+            # reset membrane potential of the neurons (in mV)
+            'E_L': -65.0,
+            # threshold potential of the neurons (in mV)
+            'V_th': -50.0,
+            # membrane potential after a spike (in mV)
+            'V_reset': -65.0,
         }
     },
+    # connection weights (the first index corresponds to the targets
+    # and the second to the sources)
+    "conn_weights": np.array([
+            [-10, 10, 10],
+            [-10, 10, 10],
+            [-20, 10, 10]
+        ])
+
+    # "connections": {
+    #     "smooth_cells": {
+    #         "smooth_cells": -10, # inhibitory connection
+    #         "P23": -10, # inhibitory connection
+    #         "P56": -20, # stronger inhibitory connection
+    #     },
+    #     "P23": {
+    #         "smooth_cells": 10, # excitatory connection
+    #         "P23": 10, # excitatory connection
+    #         "P56": 10, # excitatory connection
+    #     },
+    #     "P56": {
+    #         "smooth_cells": 10, # excitatory connection
+    #         "P23": 10, # excitatory connection
+    #         "P56": 10, # excitatory connection
+    #     },
+    #     "thalamus": {
+    #         "smooth_cells": 10, # excitatory connection
+    #         "P23": 10, # excitatory connection
+    #         "P56": 5, # weak connection
+    #     }
+    # }   
 }
     
--- assets/douglas/sim_params.py
+++ assets/douglas/sim_params.py
@@ -1,6 +1,28 @@
 sim_dict = {
+    # turn thalamic input on or off (True or False)
+    'thalamic_input': True,
+    # start of the thalamic input (in ms)
+    'th_start': 700.0,
+    # duration of the thalamic input (in ms)
+    'th_duration': 10.0,
+    # rate of the thalamic input (in spikes/s)
+    'th_rate': 120.0,
+    # number of thalamic neurons
+    'num_th_neurons': 902,
+    # connection  of the thalamus to the different populations
+    # (same order as in 'populations' in 'net_dict')
+    'conn_weights_th':
+        np.array([10, 10, 5]),
     # list of recording devices, default is 'spike_recorder'. A 'voltmeter' can
     # be added to record membrane voltages of the neurons. Nothing will be
     # recorded if an empty list is given.
-    'rec_dev': ['spike_recorder', 'voltmeter']
+    'rec_dev': ['spike_recorder', 'voltmeter'],
+    # mean amplitude of the thalamic postsynaptic potential (in mV),
+    # standard deviation will be taken from 'net_dict'
+    'PSP_th': 0.15,
+    # mean delay of the thalamic input (in ms)
+    'delay_th_mean': 1.5,
+    # relative standard deviation of the thalamic delay (in ms)
+    'delay_th_rel_std': 0.5,
+    
 }
