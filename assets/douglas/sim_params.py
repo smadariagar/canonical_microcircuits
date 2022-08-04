3a24ba24edb06aa2d3a65af51efc4e5e20266452
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2022-08-04 16:28:11 -0400

    minor changes

--- assets/douglas/sim_params.py
+++ assets/douglas/sim_params.py
@@ -14,7 +14,7 @@ sim_dict = {
     # recorded if an empty list is given.
     'rec_dev': ['spike_recorder', 'voltmeter'],
     # path to save the output data
-    'data_path': os.path.join(os.getcwd(), 'data_douglas/'),
+    'data_path': os.path.join(os.getcwd(), 'data/data_douglas/'),
     # Seed for NEST
     'rng_seed': 55,
     # number of threads per MPI process
