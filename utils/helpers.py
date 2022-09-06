c46e55154c1879618794d6fb8a47b97cf126c1dd
Camilo-UbuntuTUF <camilo.jara@ug.uchile.cl>
2022-09-06 15:59:20 -0400

    adding draw method from networkx and plot network, also fixing shared axis

--- utils/helpers.py
+++ utils/helpers.py
@@ -303,22 +303,67 @@ def plot_voltages(path, name, begin, end, populations, firing_rates_name=None, i
             axs[i].plot(firing_rates, -50*np.ones(len(firing_rates)), f"k.", label=f"FR {populations[i]}")
         axs[i].grid()
         axs[i].legend()
+        axs[i].set_ylim(-66, -49)
+        axs[i].set_ylabel('voltage [mV]')
     if input_names:
         for j, input_name in enumerate(input_names):
             for k in range(len(sd_names)):
                 if input_name == "dc_input":
-                    times, variable = data_input[input_name][k]['time_ms'], data_input[input_name][k]['I']
-                    axs[i+1].plot(times, variable, f"C{k}", label=f"{input_name}_{populations[k]}")
+                    times, var = data_input[input_name][k]['time_ms'], data_input[input_name][k]['I']
+                    axs[i+1].plot(times, var, "r", 
+                    label="dc input" if k == 0 else "")
                 else:
-                    firing_rates = pd.DataFrame(data_input[input_name][k]).values[:, 1]
-                    print(k, firing_rates)
-                    axs[i+1].plot(firing_rates, -50*np.ones(len(firing_rates)), "k.", label=f"{input_name}_{populations[k]}")
+                    var = pd.DataFrame(data_input[input_name][k]).values[:, 1]
+                    axs[i+1].plot(var, -50*np.ones(len(var)), "k.",
+                    label="poisson input" if k == 0 else "")
         axs[i+1].grid()
         axs[i+1].legend()
+        axs[i+1].set_ylabel('input [pA]')
     fig.supxlabel('time [ms]', fontsize=fs)
-    fig.supylabel('voltage [mV]', fontsize=fs)
+    #fig.supylabel('voltage [mV]', fontsize=fs)
     fig.savefig(os.path.join(path, 'voltage_plot.png'), dpi=300)
 
+def plot_network(path, populations, conn_weights, conn_weights_th=None):
+    """ Plots the network structure.
+
+    Parameters
+    -----------
+    path
+        Path where the spike times are stored.
+    populations
+        List of populations names.
+    conn_weights
+        List of connection weights.
+    conn_weights_th
+        List of connection weights for the thresholded network.
+
+    Returns
+    -------
+    None
+
+    """
+    import networkx as nx 
+
+    fs = 12  # fontsize
+    
+    # Generate network graph and params
+    G = nx.DiGraph(np.array(conn_weights))
+    G = nx.relabel_nodes(G, {i: populations[i] for i in range(len(populations))})
+    pos = nx.spring_layout(G, k=len(populations))  # For better example looking
+    if conn_weights_th is not None:
+        G.add_node("thalamic")
+        pos = {**pos, "thalamic": (0, 0)}
+        for i, w in enumerate(conn_weights_th):
+            G.add_edge("thalamic", populations[i], weight=w)
+    labels = nx.get_edge_attributes(G, 'weight')
+    # Plot the network
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    nx.draw(G, pos, with_labels=True, ax=ax)
+    draw_networkx_edge_labels(G, pos, edge_labels=labels, font_size=fs, label_pos=0.3, ax=ax)
+    
+
+    ax.set_title('Network structure', fontsize=fs)
+    fig.savefig(os.path.join(path, 'network_plot.png'), dpi=300)
 
 def firing_rates(path, name, begin, end):
     """ Computes mean and standard deviation of firing rates per population.
@@ -505,3 +550,174 @@ def __load_meter_data(path, name, begin, end):
         high = np.searchsorted(data_i_raw['time_ms'], v=end, side='right')
         data[i] = data_i_raw[low:high]
     return sd_names, node_ids, data
+
+def draw_networkx_edge_labels(
+    G,
+    pos,
+    edge_labels=None,
+    label_pos=0.5,
+    font_size=10,
+    font_color="k",
+    font_family="sans-serif",
+    font_weight="normal",
+    alpha=None,
+    bbox=None,
+    horizontalalignment="center",
+    verticalalignment="center",
+    ax=None,
+    rotate=True,
+    clip_on=True,
+):
+    """Draw edge labels.
+
+    Parameters
+    ----------
+    G : graph
+        A networkx graph
+
+    pos : dictionary
+        A dictionary with nodes as keys and positions as values.
+        Positions should be sequences of length 2.
+
+    edge_labels : dictionary (default=None)
+        Edge labels in a dictionary of labels keyed by edge two-tuple.
+        Only labels for the keys in the dictionary are drawn.
+
+    label_pos : float (default=0.5)
+        Position of edge label along edge (0=head, 0.5=center, 1=tail)
+
+    font_size : int (default=10)
+        Font size for text labels
+
+    font_color : string (default='k' black)
+        Font color string
+
+    font_weight : string (default='normal')
+        Font weight
+
+    font_family : string (default='sans-serif')
+        Font family
+
+    alpha : float or None (default=None)
+        The text transparency
+
+    bbox : Matplotlib bbox, optional
+        Specify text box properties (e.g. shape, color etc.) for edge labels.
+        Default is {boxstyle='round', ec=(1.0, 1.0, 1.0), fc=(1.0, 1.0, 1.0)}.
+
+    horizontalalignment : string (default='center')
+        Horizontal alignment {'center', 'right', 'left'}
+
+    verticalalignment : string (default='center')
+        Vertical alignment {'center', 'top', 'bottom', 'baseline', 'center_baseline'}
+
+    ax : Matplotlib Axes object, optional
+        Draw the graph in the specified Matplotlib axes.
+
+    rotate : bool (deafult=True)
+        Rotate edge labels to lie parallel to edges
+
+    clip_on : bool (default=True)
+        Turn on clipping of edge labels at axis boundaries
+
+    Returns
+    -------
+    dict
+        `dict` of labels keyed by edge
+
+    Examples
+    --------
+    >>> G = nx.dodecahedral_graph()
+    >>> edge_labels = nx.draw_networkx_edge_labels(G, pos=nx.spring_layout(G))
+
+    Also see the NetworkX drawing examples at
+    https://networkx.org/documentation/latest/auto_examples/index.html
+
+    See Also
+    --------
+    draw
+    draw_networkx
+    draw_networkx_nodes
+    draw_networkx_edges
+    draw_networkx_labels
+    """
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    if ax is None:
+        ax = plt.gca()
+    if edge_labels is None:
+        labels = {(u, v): d for u, v, d in G.edges(data=True)}
+    else:
+        labels = edge_labels
+        # Informative exception for multiedges
+        try:
+            (u, v) = next(iter(labels))  # ensures no edge key provided
+        except ValueError as err:
+            raise nx.NetworkXError(
+                "draw_networkx_edge_labels does not support multiedges."
+            ) from err
+        except StopIteration:
+            pass
+
+    text_items = {}
+    for (n1, n2), label in labels.items():
+        (x1, y1) = pos[n1]
+        (x2, y2) = pos[n2]
+        (x, y) = (
+            x1 * label_pos + x2 * (1.0 - label_pos),
+            y1 * label_pos + y2 * (1.0 - label_pos),
+        )
+
+        if rotate:
+            # in degrees
+            angle = np.arctan2(y2 - y1, x2 - x1) / (2.0 * np.pi) * 360
+            # make label orientation "right-side-up"
+            if angle > 90:
+                angle -= 180
+            if angle < -90:
+                angle += 180
+            # transform data coordinate angle to screen coordinate angle
+            xy = np.array((x, y))
+            trans_angle = ax.transData.transform_angles(
+                np.array((angle,)), xy.reshape((1, 2))
+            )[0]
+        else:
+            trans_angle = 0.0
+        # use default box of white with white border
+        if bbox is None:
+            bbox = dict(boxstyle="round", ec=(1.0, 1.0, 1.0), fc=(1.0, 1.0, 1.0))
+        if not isinstance(label, str):
+            label = str(label)  # this makes "1" and 1 labeled the same
+        print(n1, n2, label, x, y)
+        if n1 == n2:
+            y += 0.075
+        t = ax.text(
+            x,
+            y,
+            label,
+            size=font_size,
+            color=font_color,
+            family=font_family,
+            weight=font_weight,
+            alpha=alpha,
+            horizontalalignment=horizontalalignment,
+            verticalalignment=verticalalignment,
+            rotation=trans_angle,
+            transform=ax.transData,
+            bbox=bbox,
+            zorder=1,
+            clip_on=clip_on,
+        )
+        text_items[(n1, n2)] = t
+
+    ax.tick_params(
+        axis="both",
+        which="both",
+        bottom=False,
+        left=False,
+        labelbottom=False,
+        labelleft=False,
+    )
+
+    return text_items
