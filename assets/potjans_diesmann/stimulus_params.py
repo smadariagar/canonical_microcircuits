d5940dc6ab1248bc12cd85aedc2f7f3acfb0cdb7
Camilo-UbuntuPC <camilo.jara@ug.uchile.cl>
2023-12-01 10:35:28 -0300

    minor changes

--- assets/potjans_diesmann/stimulus_params.py
+++ assets/potjans_diesmann/stimulus_params.py
@@ -36,9 +36,9 @@ stim_dict = {
     # turn thalamic input on or off (True or False)
     'thalamic_input': True,
     # start of the thalamic input (in ms)
-    'th_start': 700.0,
+    'th_start': 300.0,
     # duration of the thalamic input (in ms)
-    'th_duration': 100.0,
+    'th_duration': 300.0,
     # rate of the thalamic input (in spikes/s)
     'th_rate': 120.0,
     # number of thalamic neurons
