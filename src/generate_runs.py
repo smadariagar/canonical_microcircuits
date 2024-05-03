9e5b269dfdfe0cc9cfc1b65c9833627c2bb2132c
Samuel Madariaga Román <smadariagar@ug.uchile.cl>
2024-05-03 15:19:35 -0400

    os sort issue

--- src/generate_runs.py
+++ src/generate_runs.py
@@ -7,5 +7,32 @@ warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
 
-    for i in range(20):
-        os.system("python -m src.run_connected_4_microcircuits")
+    path = "/home/samuelmr/Documentos/NEST/canonical_microcircuits/results/potjans_diesmann/20240503151459/"
+    name = 'spike_recorder'
+
+    sd_files = []
+    sd_names = []
+    for fn in os.listdir(path):
+        if fn.startswith(name):
+            sd_files.append(fn)
+            # spike recorder name and its ID
+            fnsplit = '-'.join(fn.split('-')[:-1])
+            if fnsplit not in sd_names:
+                sd_names.append(fnsplit)
+
+    print(sd_files)
+    print('*********************************')
+    #sd_files = [os.path.join(path, f) for f in sd_files] # add path to each file
+    #sd_files.sort(key=lambda x: os.path.getmtime(x))
+    #print(sd_files)
+    
+    
+    
+    
+    sd_files.sort(key=lambda fn: os.path.getmtime(os.path.join(path, fn)))
+
+    print(sd_files)
+
+    for i in range(1):
+        print(i)
+        #os.system("python -m src.run_connected_4_microcircuits")
