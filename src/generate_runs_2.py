37fb7e0fc0f5d68bfea5df05882c5f60d7aafa2a
Samuel Madariaga Román <smadariagar@ug.uchile.cl>
2025-05-28 03:08:05 -0400

    comparition ns 03.2

--- src/generate_runs_2.py
+++ src/generate_runs_2.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     folder_path = os.path.join(os.getcwd(), 'results/potjans_diesmann/')
 
     for j in range(1):
-        for k in range(10):
+        for k in range(1):
             os.system("python -m src.run_model_busse_params "+str(j)+" "+str(k))
 
 #  python -m src.generate_runs_2
