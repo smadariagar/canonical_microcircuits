b6fefa92d979bee6ea0ed77317ea2de765433590
Samuel Madariaga Román <smadariagar@ug.uchile.cl>
2025-07-18 14:47:17 -0400

    pso

--- src/generate_runs_2.py
+++ src/generate_runs_2.py
@@ -22,13 +22,13 @@ if __name__ == '__main__':
 
     folder_path = os.path.join(os.getcwd(), 'results/potjans_diesmann/')
 
-    # for j in range(7):
-    #     for k in range(1):
-    #         os.system("python -m src.run_model_busse_params "+str(j)+" "+str(k))
+    for j in range(1):
+        for k in range(2):
+            os.system("python -m src.run_model_busse_params "+str(j)+" "+str(k))
 
 
-    carpetas = [nombre for nombre in os.listdir(folder_path)
-            if os.path.isdir(os.path.join(folder_path, nombre))]
-    for carpeta in carpetas:
+    # carpetas = [nombre for nombre in os.listdir(folder_path)
+    #         if os.path.isdir(os.path.join(folder_path, nombre))]
+    # for carpeta in carpetas:
        
-        os.system("python -m src.lfp "+carpeta)
+    #     os.system("python -m src.lfp "+carpeta)
