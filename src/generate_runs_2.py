8502b9e1143b3ecf5cbf0bdebf253709924035af
Samuel Madariaga Román <smadariagar@ug.uchile.cl>
2025-10-19 00:24:50 -0300

    pre defensa 2

--- src/generate_runs_2.py
+++ src/generate_runs_2.py
@@ -22,9 +22,10 @@ if __name__ == '__main__':
 
     folder_path = os.path.join(os.getcwd(), 'results/potjans_diesmann/')
 
-    for j in range(1):
-        for k in range(2):
-            os.system("python -m src.run_model_busse_params "+str(j)+" "+str(k))
+    for j in range(3):
+        for k in range(3):
+            for l in range(10):
+                os.system("python -m src.run_model_busse_params "+str(j)+" "+str(k))
 
 
     # carpetas = [nombre for nombre in os.listdir(folder_path)
--- src/run_model_busse_params.py
+++ src/run_model_busse_params.py
@@ -95,8 +95,8 @@ if __name__ == '__main__':
     print('---------> Creating the model...')
 
     # N & K scaling
-    ns = 0.1#(args.gen+1)/10
-    ks = 0.1#(args.gen+5)/10
+    ns = 0.2#(args.gen+1)/10
+    ks = 0.2#(args.gen+5)/10
 
     net_dict.update({'N_scaling': ns})
     net_dict.update({'K_scaling': ks})
@@ -163,14 +163,29 @@ if __name__ == '__main__':
     stim_dict_D = stim_dict_A.copy()
     stim_dict_V2 = stim_dict_A.copy()
 
+    if args.gen == 0:
+        TR = 100.0
+    elif args.gen == 1:
+        TR = 200.0
+    elif args.gen == 2:
+        TR = 300.0
+
+    if args.id_s == 0:
+        TD = 100.0
+    elif args.id_s == 1:
+        TD = 200.0
+    elif args.id_s == 2:
+        TD = 300.0
+
     # Stimulation to MCC A
+    
     stim_star = 500.0 
     stim_duration = 1000.0
 
     stim_dict_A.update({'thalamic_input': True})
     stim_dict_A.update({'th_start': stim_star})
     stim_dict_A.update({'th_duration': stim_duration})
-    stim_dict_A.update({'th_rate': 200.0})
+    stim_dict_A.update({'th_rate': TR})
     #stim_dict_A.update({'num_th_neurons': 0})
 
     # Stimulation to MCC B
@@ -187,27 +202,21 @@ if __name__ == '__main__':
     stim_star = 1000.0 
     stim_duration = 500.0
 
-    # if args.gen == 0:
-    #     TR = 200.0
-    # elif args.gen == 1:
-    #     TR = 100.0
-    # # elif args.gen == 2:
-    # #     TR = 300.0
-
-    stim_dict_C.update({'thalamic_input': False})
+    stim_dict_C.update({'thalamic_input': True})
     stim_dict_C.update({'th_start': stim_star})
     stim_dict_C.update({'th_duration': stim_duration})
-    stim_dict_C.update({'th_rate': 200.0})
-    stim_dict_C.update({'num_th_neurons': 0})
+    stim_dict_C.update({'th_rate': TD})
+    #stim_dict_C.update({'num_th_neurons': 0})
 
     # Stimulation to MCC D
     stim_star = 1000.0 
     stim_duration = 500.0
 
-    stim_dict_D.update({'thalamic_input': True})
+    stim_dict_D.update({'thalamic_input': False})
     stim_dict_D.update({'th_start': stim_star})
     stim_dict_D.update({'th_duration': stim_duration})
     stim_dict_D.update({'th_rate': 300.0})
+    stim_dict_D.update({'num_th_neurons': 0})
 
     ###############################################################################
     # Model type
