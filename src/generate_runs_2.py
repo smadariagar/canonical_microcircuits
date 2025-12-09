c778b13a59cb17f8446d101b26e9748deddf6338
Samuel Madariaga Román <smadariagar@ug.uchile.cl>
2025-12-09 20:05:32 -0300

    busse publica

--- src/generate_runs_2.py
+++ src/generate_runs_2.py
@@ -22,9 +22,10 @@ if __name__ == '__main__':
 
     folder_path = os.path.join(os.getcwd(), 'results/potjans_diesmann/')
 
-    for j in range(1):
-        for k in range(10):
-            os.system("python -m src.run_model_busse_params "+str(2)+" "+str(k))
+    for j in range(4):
+        for k in range(4):
+            for l in range(10):
+                os.system("python -m src.run_model_busse_params "+str(j)+" "+str(k))
 
 
     # carpetas = [nombre for nombre in os.listdir(folder_path)
