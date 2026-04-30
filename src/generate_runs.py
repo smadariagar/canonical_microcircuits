9207a0aaa5bb1f0a985d3db07bcc6c8087c2b40e
Samuel Madariaga <smadariagar@ug.uchile.cl>
2026-04-30 14:54:55 -0400

    nlhpc atualization

--- src/generate_runs.py
+++ src/generate_runs.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     folder_path = os.path.join(os.getcwd(), 'results/potjans_diesmann/')
 
     for j in range(1):
-        for k in [125.0, 175.0, 250.0]:
+        for k in [100.0, 200.0, 300.0]:
             for l in range(10):
                 comando = ["python", "-m", "src.run_experiment", 
                            "--ns", str(0.2), "--ks", str(0.2), 
